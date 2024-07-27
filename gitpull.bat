f8e373220c30552edd7c39deb793be679141bc3b
Renée Maksoud <renee.maksoud@correiarodrigues.com.br>
2024-07-27 14:59:52 -0300

    Atualiza├º├úo autom├ítica de notas

---
+++ gitpull.bat
@@ -0,0 +1,18 @@
+@echo off
+:: Define the path to your repository
+set REPO_PATH=C:\Users\renee\OneDrive\Documentos\Obsidian Vault\maksoud.github.io
+
+:: Navigate to the repository directory
+cd /d %REPO_PATH%
+
+:: Add all changes to git
+git add .
+
+:: Commit changes with a message
+git commit -m "Atualização automática de notas"
+
+:: Push changes to the remote repository
+git push origin main
+
+:: Pause to view the output
+pause
